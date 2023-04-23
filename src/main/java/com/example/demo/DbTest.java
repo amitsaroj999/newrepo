a93f32f76b2915820e1d998bdfc1b08a79122cca
amitsaroj999 <amitsaroj999@gmail.com>
2023-04-23 18:19:57 +0530

    branch3

--- src/main/java/com/example/demo/DbTest.java
+++ src/main/java/com/example/demo/DbTest.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 public class DbTest {
     public static void main(String[] args) {
-        System.out.println("Branch1");
-        System.out.println("Branch2");
+        System.out.println("Branch3");
     }
 }
