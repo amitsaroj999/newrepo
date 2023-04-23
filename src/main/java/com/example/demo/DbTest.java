64f147da1b3c40900482efea14f12b8119dcb129
amitsaroj999 <amitsaroj999@gmail.com>
2023-04-23 18:17:33 +0530

    test1

--- src/main/java/com/example/demo/DbTest.java
+++ src/main/java/com/example/demo/DbTest.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class DbTest {
     public static void main(String[] args) {
-        System.out.println("Enter");
+        System.out.println("Branch1");
     }
 }
